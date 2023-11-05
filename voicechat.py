421559f20b912123ee546bd90436b0b434d7ed9b
Flooperr <149349356+Flooperr@users.noreply.github.com>
2023-11-05 00:50:58 -0400

    Update voicechat.py

--- voicechat.py
+++ voicechat.py
@@ -1,19 +1,4 @@
 import youtube_dl
 
-# Define a function to download MP3
-def download_mp3(query):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(query, download=False)
-        video_url = info_dict['url']
-        ydl.download([video_url])
-
-
+# COMING SOON
